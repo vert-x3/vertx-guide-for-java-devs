36b3446c72a1b35597ef135a73f8ceb98a4ae5fc
Julien Ponge <julien.ponge@insa-lyon.fr>
2016-11-18 15:06:34 +0100

    Start step-3

---
+++ step-3/src/main/java/io/vertx/guides/wiki/HttpServerVerticle.java
@@ -0,0 +1,195 @@
+/*
+ *  Copyright (c) 2016 Red Hat, Inc. and/or its affiliates.
+ *  Copyright (c) 2016 INSA Lyon, CITI Laboratory.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.vertx.guides.wiki;
+
+import com.github.rjeschke.txtmark.Processor;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.templ.FreeMarkerTemplateEngine;
+
+import java.util.Date;
+
+/**
+ * @author <a href="https://julien.ponge.org/">Julien Ponge</a>
+ */
+public class HttpServerVerticle extends AbstractVerticle {
+
+  public static final String CONFIG_HTTP_SERVER_PORT = "http.server.port";
+  public static final String CONFIG_WIKIDB_QUEUE = "wikidb.queue";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerVerticle.class);
+
+  private final FreeMarkerTemplateEngine templateEngine = FreeMarkerTemplateEngine.create();
+
+  private String wikiDbQueue = "wikidb.queue";
+
+  private static final String EMPTY_PAGE_MARKDOWN =
+    "# A new page\n" +
+      "\n" +
+      "Feel-free to write in Markdown!\n";
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+
+    wikiDbQueue = config().getString(CONFIG_WIKIDB_QUEUE, "wikidb.queue");
+
+    HttpServer server = vertx.createHttpServer();
+
+    Router router = Router.router(vertx);
+    router.get("/").handler(this::indexHandler);
+    router.get("/wiki/:page").handler(this::pageRenderingHandler);
+    router.post().handler(BodyHandler.create());
+    router.post("/save").handler(this::pageUpdateHandler);
+    router.post("/create").handler(this::pageCreateHandler);
+    router.post("/delete").handler(this::pageDeletionHandler);
+
+    int portNumber = config().getInteger(CONFIG_HTTP_SERVER_PORT, 8080);
+    server
+      .requestHandler(router::accept)
+      .listen(portNumber, ar -> {
+        if (ar.succeeded()) {
+          LOGGER.info("HTTP server running on port " + portNumber);
+          startFuture.complete();
+        } else {
+          LOGGER.error("Could not start a HTTP server", ar.cause());
+          startFuture.fail(ar.cause());
+        }
+      });
+  }
+
+
+  private void indexHandler(RoutingContext context) {
+
+    DeliveryOptions options = new DeliveryOptions().addHeader("action", "all-pages");
+
+    vertx.eventBus().send(wikiDbQueue, new JsonObject(), options, reply -> {
+      if (reply.succeeded()) {
+        JsonObject body = (JsonObject) reply.result().body();
+        context.put("title", "Wiki home");
+        context.put("pages", body.getJsonArray("pages").getList());
+        templateEngine.render(context, "templates/index.ftl", ar -> {
+          if (ar.succeeded()) {
+            context.response().putHeader("Content-Type", "text/html");
+            context.response().end(ar.result());
+          } else {
+            context.fail(ar.cause());
+          }
+        });
+      } else {
+        context.fail(reply.cause());
+      }
+    });
+  }
+
+  private void pageRenderingHandler(RoutingContext context) {
+
+    String requestedPage = context.request().getParam("page");
+    JsonObject request = new JsonObject().put("page", requestedPage);
+
+    DeliveryOptions options = new DeliveryOptions().addHeader("action", "get-page");
+    vertx.eventBus().send(wikiDbQueue, request, options, reply -> {
+
+      if (reply.succeeded()) {
+        JsonObject body = (JsonObject) reply.result().body();
+
+        boolean found = body.getBoolean("found");
+        String rawContent = body.getString("rawContent", EMPTY_PAGE_MARKDOWN);
+        context.put("title", requestedPage);
+        context.put("id", body.getInteger("id", -1));
+        context.put("newPage", found ? "no" : "yes");
+        context.put("rawContent", rawContent);
+        context.put("content", Processor.process(rawContent));
+        context.put("timestamp", new Date().toString());
+
+        templateEngine.render(context, "templates/page.ftl", ar -> {
+          if (ar.succeeded()) {
+            context.response().putHeader("Content-Type", "text/html");
+            context.response().end(ar.result());
+          } else {
+            context.fail(ar.cause());
+          }
+        });
+
+      } else {
+        context.fail(reply.cause());
+      }
+    });
+  }
+
+  private void pageUpdateHandler(RoutingContext context) {
+
+    String title = context.request().getParam("title");
+    JsonObject request = new JsonObject()
+      .put("id", context.request().getParam("id"))
+      .put("title", title)
+      .put("markdown", context.request().getParam("markdown"));
+
+    DeliveryOptions options = new DeliveryOptions();
+    if ("yes".equals(context.request().getParam("newPage"))) {
+      options.addHeader("action", "create-page");
+    } else {
+      options.addHeader("action", "save-page");
+    }
+
+    vertx.eventBus().send(wikiDbQueue, request, options, reply -> {
+      if (reply.succeeded()) {
+        context.response().setStatusCode(303);
+        context.response().putHeader("Location", "/wiki/" + title);
+        context.response().end();
+      } else {
+        context.fail(reply.cause());
+      }
+    });
+  }
+
+  private void pageCreateHandler(RoutingContext context) {
+    String pageName = context.request().getParam("name");
+    String location = "/wiki/" + pageName;
+    if (pageName == null || pageName.isEmpty()) {
+      location = "/";
+    }
+    context.response().setStatusCode(303);
+    context.response().putHeader("Location", location);
+    context.response().end();
+  }
+
+  private void pageDeletionHandler(RoutingContext context) {
+    String id = context.request().getParam("id");
+    JsonObject request = new JsonObject().put("id", id);
+    DeliveryOptions options = new DeliveryOptions().addHeader("action", "delete-page");
+    vertx.eventBus().send(wikiDbQueue, request, options, reply -> {
+      if (reply.succeeded()) {
+        context.response().setStatusCode(303);
+        context.response().putHeader("Location", "/");
+        context.response().end();
+      } else {
+        context.fail(reply.cause());
+      }
+    });
+  }
+}
---
+++ step-3/src/main/resources/templates/header.ftl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css"
+        integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+  <title>${context.title} | A Sample Vert.x-powered Wiki</title>
+</head>
+<body>
+
+<div class="container">
