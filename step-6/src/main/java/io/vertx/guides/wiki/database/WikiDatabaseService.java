400c3932461f7d90deba6c0d4e165abe91c940ea
Julien Ponge <julien.ponge@gmail.com>
2016-11-30 12:39:53 +0100

    Web API start

---
+++ step-6/src/main/java/io/vertx/guides/wiki/database/WikiDatabaseService.java
@@ -0,0 +1,66 @@
+/*
+ *  Copyright (c) 2016 Red Hat, Inc. and/or its affiliates.
+ *  Copyright (c) 2016 INSA Lyon, CITI Laboratory.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.vertx.guides.wiki.database;
+
+import io.vertx.codegen.annotations.Fluent;
+import io.vertx.codegen.annotations.ProxyGen;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author <a href="https://julien.ponge.org/">Julien Ponge</a>
+ */
+@ProxyGen
+public interface WikiDatabaseService {
+
+  static WikiDatabaseService create(JDBCClient dbClient, HashMap<SqlQuery, String> sqlQueries, Handler<AsyncResult<WikiDatabaseService>> readyHandler) {
+    return new WikiDatabaseServiceImpl(dbClient, sqlQueries, readyHandler);
+  }
+
+  static WikiDatabaseService createProxy(Vertx vertx, String address) {
+    return new WikiDatabaseServiceVertxEBProxy(vertx, address);
+  }
+
+  @Fluent
+  WikiDatabaseService fetchAllPages(Handler<AsyncResult<JsonArray>> resultHandler);
+
+  @Fluent
+  WikiDatabaseService fetchPage(String name, Handler<AsyncResult<JsonObject>> resultHandler);
+
+  @Fluent
+  WikiDatabaseService fetchPageById(int id, Handler<AsyncResult<JsonObject>> resultHandler);
+
+  @Fluent
+  WikiDatabaseService createPage(String title, String markdown, Handler<AsyncResult<Void>> resultHandler);
+
+  @Fluent
+  WikiDatabaseService savePage(int id, String markdown, Handler<AsyncResult<Void>> resultHandler);
+
+  @Fluent
+  WikiDatabaseService deletePage(int id, Handler<AsyncResult<Void>> resultHandler);
+
+  @Fluent
+  WikiDatabaseService fetchAllPagesData(Handler<AsyncResult<List<JsonObject>>> resultHandler);
+}
---
+++ step-6/src/test/java/io/vertx/guides/wiki/http/SampleHttpServerTest.java
@@ -0,0 +1,74 @@
+/*
+ *  Copyright (c) 2016 Red Hat, Inc. and/or its affiliates.
+ *  Copyright (c) 2016 INSA Lyon, CITI Laboratory.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.vertx.guides.wiki.http;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="https://julien.ponge.org/">Julien Ponge</a>
+ */
+@RunWith(VertxUnitRunner.class)
+public class SampleHttpServerTest {
+
+  private Vertx vertx;
+
+  @Before
+  public void prepare() {
+    vertx = Vertx.vertx();
+  }
+
+  @After
+  public void finish(TestContext context) {
+    vertx.close(context.asyncAssertSuccess());
+  }
+
+  @Test
+  public void start_http_server(TestContext context) {
+    Async async = context.async();
+
+    vertx
+      .createHttpServer().requestHandler(req ->
+      req.response().putHeader("Content-Type", "text/plain").end("Ok"))
+      .listen(8080, context.asyncAssertSuccess(server -> {
+
+        HttpClient httpClient = vertx.createHttpClient();
+
+        httpClient.get(8080, "localhost", "/", response -> {
+          response.exceptionHandler(throwable -> async.resolve(Future.failedFuture(throwable)));
+          response.bodyHandler(body -> {
+            context.assertTrue(response.headers().contains("Content-Type"));
+            context.assertEquals("text/plain", response.getHeader("Content-Type"));
+            context.assertEquals("Ok", body.toString());
+            httpClient.close();
+            async.complete();
+          });
+        }).exceptionHandler(throwable -> async.resolve(Future.failedFuture(throwable)))
+          .end();
+
+      }));
+  }
+}
