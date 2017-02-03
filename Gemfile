5ad86a30631fb3de33007b54161b3d6d0e1c8eea
Julien Ponge <julien.ponge@insa-lyon.fr>
2017-02-03 19:01:23 +0100

    Not using asciidoctor-diagram as of yet

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source "https://rubygems.org"
 
 gem 'asciidoctor'
-gem 'asciidoctor-diagram'
--- process-asciidoctor.sh
+++ process-asciidoctor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bundle exec asciidoctor -o guide.html -r asciidoctor-diagram README.adoc
+bundle exec asciidoctor -o guide.html README.adoc
