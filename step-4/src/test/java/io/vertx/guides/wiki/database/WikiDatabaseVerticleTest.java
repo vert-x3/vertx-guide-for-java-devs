a21834114f4cb95e07659a35b6e4a3a94853b7af
Julien Ponge <julien.ponge@insa-lyon.fr>
2017-02-09 23:25:33 +0100

    step-4

--- step-4/src/test/java/io/vertx/guides/wiki/database/WikiDatabaseVerticleTest.java
+++ step-4/src/test/java/io/vertx/guides/wiki/database/WikiDatabaseVerticleTest.java
@@ -34,25 +34,44 @@ import org.junit.runner.RunWith;
 @RunWith(VertxUnitRunner.class)
 public class WikiDatabaseVerticleTest {
 
+  // tag::prepare[]
   private Vertx vertx;
   private WikiDatabaseService service;
 
   @Before
   public void prepare(TestContext context) throws InterruptedException {
     vertx = Vertx.vertx();
-    JsonObject conf = new JsonObject()
+    
+    JsonObject conf = new JsonObject()  // <1>
       .put(WikiDatabaseVerticle.CONFIG_WIKIDB_JDBC_URL, "jdbc:hsqldb:mem:testdb;shutdown=true")
       .put(WikiDatabaseVerticle.CONFIG_WIKIDB_JDBC_MAX_POOL_SIZE, 4);
+
     vertx.deployVerticle(new WikiDatabaseVerticle(), new DeploymentOptions().setConfig(conf),
-      context.asyncAssertSuccess(id ->
+      context.asyncAssertSuccess(id ->  // <2>
         service = WikiDatabaseService.createProxy(vertx, WikiDatabaseVerticle.CONFIG_WIKIDB_QUEUE)));
   }
+  // end::prepare[]
 
+  // tag::finish[]
   @After
   public void finish(TestContext context) {
     vertx.close(context.asyncAssertSuccess());
   }
+  // end::finish[]
 
+  // tag::async-basic[]
+  @Test /*(timeout=5000)*/  // <8>
+  public void async_behavior(TestContext context) { // <1>
+    Vertx vertx = Vertx.vertx();  // <2>
+    context.assertEquals("foo", "foo");  // <3>
+    Async a1 = context.async();   // <4>
+    Async a2 = context.async(3);  // <5>
+    vertx.setTimer(100, n -> a1.complete());  // <6>
+    vertx.setPeriodic(100, n -> a2.countDown());  // <7>
+  }
+  // end::async-basic[]
+
+  // tag::crud[]
   @Test
   public void crud_operations(TestContext context) {
     Async async = context.async();
@@ -76,7 +95,7 @@ public class WikiDatabaseVerticleTest {
 
                 service.fetchAllPages(context.asyncAssertSuccess(array2 -> {
                   context.assertTrue(array2.isEmpty());
-                  async.complete();
+                  async.complete();  // <1>
                 }));
               });
             }));
@@ -84,6 +103,7 @@ public class WikiDatabaseVerticleTest {
         }));
       }));
     }));
-    async.awaitSuccess(5000);
+    async.awaitSuccess(5000); // <2>
   }
+  // end::crud[]
 }
