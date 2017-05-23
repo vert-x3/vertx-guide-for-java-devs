996865a0e9651c16679436f0b8455a44e5f0a56d
Julien Ponge <julien.ponge@insa-lyon.fr>
2017-05-23 16:39:49 +0200

    Extract adoc preamble attributes

--- process-asciidoctor.sh
+++ process-asciidoctor.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-bundle exec asciidoctor -b html -o guide.html -a icons=font README.adoc
+bundle exec asciidoctor \
+    -b html \
+    -o guide.html \
+    -a icons=font \
+    -a source-highlighter=highlight.js \
+    -a data-uri \
+    -a toc=left \
+    -a toclevels=4 \
+    -a sectnums \
+    README.adoc
