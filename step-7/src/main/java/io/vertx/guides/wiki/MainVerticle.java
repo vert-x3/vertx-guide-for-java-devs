ca04c8358a2a32de33a7471db236b5ca2de4bcaf
Julien Ponge <julien.ponge@gmail.com>
2018-04-26 14:48:34 +0200

    Replacing Apache Shiro authentication by JDBC

--- step-7/src/main/java/io/vertx/guides/wiki/MainVerticle.java
+++ step-7/src/main/java/io/vertx/guides/wiki/MainVerticle.java
@@ -21,28 +21,31 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.guides.wiki.database.WikiDatabaseVerticle;
+import io.vertx.guides.wiki.http.AuthInitializerVerticle;
 
 /**
  * @author <a href="https://julien.ponge.org/">Julien Ponge</a>
  */
+// tag::code[]
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
 
     Future<String> dbVerticleDeployment = Future.future();
-    vertx.deployVerticle(new WikiDatabaseVerticle(), dbVerticleDeployment.completer());
+    vertx.deployVerticle(new WikiDatabaseVerticle(), dbVerticleDeployment);
 
     dbVerticleDeployment.compose(id -> {
-
+      Future<String> authInitDeployment = Future.future();
+      vertx.deployVerticle(new AuthInitializerVerticle(), authInitDeployment);
+      return authInitDeployment;
+    }).compose(id -> {
       Future<String> httpVerticleDeployment = Future.future();
       vertx.deployVerticle(
         "io.vertx.guides.wiki.http.HttpServerVerticle",
         new DeploymentOptions().setInstances(2),
         httpVerticleDeployment.completer());
-
       return httpVerticleDeployment;
-
     }).setHandler(ar -> {
       if (ar.succeeded()) {
         startFuture.complete();
@@ -52,3 +55,4 @@ public class MainVerticle extends AbstractVerticle {
     });
   }
 }
+// end::code[]
