815059b4378b51fb07aa538519329d2e9dac2888
Paul Court <g@rgoyle.com>
2018-06-23 09:53:57 +0100

    Using the Fluent interface adds complexity but no real value to the concepts being explained.

--- step-3/src/main/java/io/vertx/guides/wiki/database/WikiDatabaseService.java
+++ step-3/src/main/java/io/vertx/guides/wiki/database/WikiDatabaseService.java
@@ -35,20 +35,15 @@ import java.util.HashMap;
 @ProxyGen
 public interface WikiDatabaseService {
 
-  @Fluent
-  WikiDatabaseService fetchAllPages(Handler<AsyncResult<JsonArray>> resultHandler);
+  void fetchAllPages(Handler<AsyncResult<JsonArray>> resultHandler);
 
-  @Fluent
-  WikiDatabaseService fetchPage(String name, Handler<AsyncResult<JsonObject>> resultHandler);
+  void fetchPage(String name, Handler<AsyncResult<JsonObject>> resultHandler);
 
-  @Fluent
-  WikiDatabaseService createPage(String title, String markdown, Handler<AsyncResult<Void>> resultHandler);
+  void createPage(String title, String markdown, Handler<AsyncResult<Void>> resultHandler);
 
-  @Fluent
-  WikiDatabaseService savePage(int id, String markdown, Handler<AsyncResult<Void>> resultHandler);
+  void savePage(int id, String markdown, Handler<AsyncResult<Void>> resultHandler);
 
-  @Fluent
-  WikiDatabaseService deletePage(int id, Handler<AsyncResult<Void>> resultHandler);
+  void deletePage(int id, Handler<AsyncResult<Void>> resultHandler);
 
   // (...)
   // end::interface[]
